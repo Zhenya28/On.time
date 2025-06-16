17ebe3f0cfa4cb893261dba8150e1e82acbb0de8
Zhenya28 <107507523+Zhenya28@users.noreply.github.com>
2025-06-16 13:06:42 +0200

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,7 +2,6 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB8QbK7_4YTqTmgX4JQryzEzUDPm5J_II4",
   authDomain: "ontime-4ee92.firebaseapp.com",
@@ -12,13 +11,10 @@ const firebaseConfig = {
   appId: "1:283407605194:web:e622e6e6bc28cac0f98333"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
 
-// Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
 
 export default app; 
